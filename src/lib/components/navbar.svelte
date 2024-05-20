d4394a34d12776087d7748d7290bc4fed1e4355b
hamza oubrahim <hamzaoubrahim51@gmail.com>
2024-05-20 20:58:23 +0000

    landing page setup

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.png';
+	import Button from './ui/button/button.svelte';
+</script>
+
+<nav class="">
+	<div
+		class="sticky top-0 mx-auto flex max-w-screen-xl flex-row items-center justify-between gap-2 px-8 py-6"
+	>
+		<a href="/">
+			<img src={logo} alt={'Chax logo'} width={50} />
+		</a>
+		<div class="space-x-6">
+			<a href="/about">About</a>
+			<a href="/safety">Safety</a>
+			<a href="/blog">Blog</a>
+		</div>
+		<Button class="text-white">Join</Button>
+	</div>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+
+<div class="flex flex-col gap-2 h-[80vh] item-center justify-center text-center">
+    <h1 class="font-bold text-7xl tracking-tight">Customizable, open-source<br/> chat app.</h1>
+</div>
+
+
