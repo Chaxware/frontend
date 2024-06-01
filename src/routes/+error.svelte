529a3c9aeb05d71399532ba017d96238aaa2f091
MrinmoyHaloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2024-06-01 22:02:08 +0530

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex h-[80vh] w-screen flex-col gap-2 text-center pt-28">
+    <h1 class="text-4xl sm:text-7xl font-bold tracking-tight">{$page.status}: {$page.error && $page.error.message} </h1>
+    {#if $page.status === 404}
+        <p class="text-gray-500">The page you're looking for doesn't exist.</p>
+    {:else}
+        <p class="text-gray-500">An error occurred while loading this page.</p>
+    {/if}
+</div>
