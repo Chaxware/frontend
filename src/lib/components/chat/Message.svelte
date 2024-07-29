8bc8afcb531525511efb712e469e2c95a10f115a
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2024-07-29 19:52:19 +0530

    feat: message component

---
+++ src/lib/components/chat/Message.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    let user = {
+        name: "John Doe",
+        profile: "https://mighty.tools/mockmind-api/content/human/57.jpg",
+    };
+    let message = "How you doing?";
+
+    export { user, message };
+</script>
+
+<div class="message flex">
+    <img src="{user.profile}" alt="user profile" />
+    <div class="flex flex-col">
+        <div>
+            <span class="name">{user.name}</span><span class="timestamp ms-2 text-muted"
+                >Today at 12:02 AM</span
+            >
+        </div>
+        <span class="message-content">{message}</span>
+    </div>
+</div>
+
+<style lang=scss>
+    .message {
+		gap: 1rem;
+		img {
+			width: 50px;
+			height: 50px;
+			border-radius: 50%;
+		}
+		.name {
+			font-weight: 500;
+		}
+		.timestamp {
+			color: #a6adc8;
+			font-size: 0.7rem;
+		}
+		.message-content {
+			color: #cdd6f4;
+		}
+	}
+</style>
--- src/routes/(app)/chat/+page.svelte
+++ src/routes/(app)/chat/+page.svelte
@@ -1,92 +1,93 @@
 <script lang="ts">
 	import { Paperclip, Search, Send } from 'lucide-svelte';
 	import type { PageData } from './$types';
+	import Message from '@/components/chat/Message.svelte';
 
 	export let data: PageData;
 </script>
 
 <div class="flex">
-	<div class="flex h-dvh border-r-[1px] border-gray-600 w-80 w">
+	<div class="w flex h-dvh w-80 border-r-[1px] border-gray-600">
 		<div class="border-r-[1px] border-gray-600">sidebar</div>
 		<div class="w-full">
-            <div class="border-b-[1px] border-gray-600 py-4 px-4">{`{serverName}`}</div>
-            <div class="p-2">
-                <div class="searchBox">
-                    <Search size={18} color="#A6ADC8"/>
-                    <input required placeholder="Search" type="text" id="searchInput" />
-                </div>
-            </div>
-        </div>
+			<div class="border-b-[1px] border-gray-600 px-4 py-4">{`{serverName}`}</div>
+			<div class="p-2">
+				<div class="searchBox">
+					<Search size={18} color="#A6ADC8" />
+					<input required placeholder="Search" type="text" id="searchInput" />
+				</div>
+			</div>
+		</div>
 	</div>
 	<div class="flex flex-1 flex-col">
-		<div class="border-b-[1px] border-gray-600 py-4 px-8">{`{channelName}`}</div>
-        <div class="py-4 px-8 flex flex-col flex-1">
-            <div class="flex-1">
-                messages
-            </div>
-            <div class="messageBox">
-                <div class="fileUploadWrapper">
-                  <label for="file">
-                    <Paperclip color="#A6ADC8"/>
-                  </label>
-                  <input type="file" id="file" name="file" />
-                </div>
-                <input required placeholder="Write some sweet words..." type="text" id="messageInput" />
-                <button id="sendButton">
-                    <Send color="#A6ADC8"/>
-                </button>
-              </div>
-            
-        </div>
+		<div class="border-b-[1px] border-gray-600 px-8 py-4">{`{channelName}`}</div>
+		<div class="flex flex-1 flex-col px-8 py-4">
+			<div class="flex-1 flex justify-end gap-4 pb-4 flex-col">
+                <Message message="How you doing?"/>
+                <Message message="I am doing fine. What about you?"/>
+                <Message message="I am fine too. How is the project coming along?"/>
+                <Message message="Which project?"/>
+                <Message message="Chax the chat app"/>
+			</div>
+			<div class="messageBox">
+				<div class="fileUploadWrapper">
+					<label for="file">
+						<Paperclip color="#A6ADC8" />
+					</label>
+					<input type="file" id="file" name="file" />
+				</div>
+				<input required placeholder="Write some sweet words..." type="text" id="messageInput" />
+				<button id="sendButton">
+					<Send color="#A6ADC8" />
+				</button>
+			</div>
+		</div>
 	</div>
 </div>
 
 <style lang="scss">
+	.searchBox {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 8px 10px;
+		border-radius: 4px;
+		font-size: 0.9rem;
+		background-color: #313244;
+	}
 
-.searchBox {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 8px 10px;
-  border-radius: 4px;
-  font-size: .9rem;
-  background-color: #313244;
-}
+	#searchInput {
+		width: 100%;
+		height: 100%;
+		background-color: transparent;
+		outline: none;
+		border: none;
+		padding-left: 10px;
+		color: white;
+		flex: 1;
+	}
 
-#searchInput {
-  width: 100%;
-  height: 100%;
-  background-color: transparent;
-  outline: none;
-  border: none;
-  padding-left: 10px;
-  color: white;
-  flex: 1;
-}
+	.messageBox {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 12px 20px;
+		border-radius: 4px;
+		border: 1px solid #4b5563;
+	}
 
-.messageBox {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 12px 20px;
-  border-radius: 4px;
-  border: 1px solid  #4b5563;
-}
+	#file {
+		display: none;
+	}
 
-
-#file {
-  display: none;
-}
-
-#messageInput {
-  width: 200px;
-  height: 100%;
-  background-color: transparent;
-  outline: none;
-  border: none;
-  padding-left: 10px;
-  color: white;
-  flex: 1;
-}
-
+	#messageInput {
+		width: 200px;
+		height: 100%;
+		background-color: transparent;
+		outline: none;
+		border: none;
+		padding-left: 10px;
+		color: white;
+		flex: 1;
+	}
 </style>
