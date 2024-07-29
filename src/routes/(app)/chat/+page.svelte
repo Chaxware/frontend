0238589317ff1e9c4c9cdf205348a8a747998615
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2024-07-29 19:05:07 +0530

    feat(ui): implement search field

--- src/routes/(app)/chat/+page.svelte
+++ src/routes/(app)/chat/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Paperclip, Send } from 'lucide-svelte';
+	import { Paperclip, Search, Send } from 'lucide-svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -8,7 +8,15 @@
 <div class="flex">
 	<div class="flex h-dvh border-r-[1px] border-gray-600 w-80 w">
 		<div class="border-r-[1px] border-gray-600">sidebar</div>
-		<div class="border-b-[1px] border-gray-600 h-fit w-full py-4 px-4">{`{serverName}`}</div>
+		<div class="w-full">
+            <div class="border-b-[1px] border-gray-600 py-4 px-4">{`{serverName}`}</div>
+            <div class="p-2">
+                <div class="searchBox">
+                    <Search size={18} color="#A6ADC8"/>
+                    <input required placeholder="Search" type="text" id="searchInput" />
+                </div>
+            </div>
+        </div>
 	</div>
 	<div class="flex flex-1 flex-col">
 		<div class="border-b-[1px] border-gray-600 py-4 px-8">{`{channelName}`}</div>
@@ -19,13 +27,13 @@
             <div class="messageBox">
                 <div class="fileUploadWrapper">
                   <label for="file">
-                    <Paperclip  />
+                    <Paperclip color="#A6ADC8"/>
                   </label>
                   <input type="file" id="file" name="file" />
                 </div>
                 <input required placeholder="Write some sweet words..." type="text" id="messageInput" />
                 <button id="sendButton">
-                    <Send />
+                    <Send color="#A6ADC8"/>
                 </button>
               </div>
             
@@ -34,6 +42,27 @@
 </div>
 
 <style lang="scss">
+
+.searchBox {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px 10px;
+  border-radius: 4px;
+  font-size: .9rem;
+  background-color: #313244;
+}
+
+#searchInput {
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  padding-left: 10px;
+  color: white;
+  flex: 1;
+}
 
 .messageBox {
   display: flex;
