3fbd222e20ab32d9fa4aad1905bee530a40d7a84
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2024-08-12 21:14:46 +0530

    refactor: improve messages and hubs loading performance
    Refactor hubs loading code to improve performance. This includes fetching hubs and messages in parallel to prevent a waterfall.

--- src/routes/(app)/chat/+page.ts
+++ src/routes/(app)/chat/+page.ts
@@ -1,32 +1,27 @@
 import { error, type Load } from '@sveltejs/kit';
 
-const resourceUrl = process.env.NODE_ENV === 'production' 
-? "https://chaxback.chax.workers.dev"
-: "http://localhost:8787";
+const resourceUrl =
+	process.env.NODE_ENV === 'production'
+		? 'https://chaxback.chax.workers.dev'
+		: 'http://localhost:8787';
 
 /** @type {import('@sveltejs/kit').Load} */
-export const load: Load = async ({ fetch }) => {    
-    
-    const chatRequest = async () => {
-        const chatResourceUrl = `${resourceUrl}/chat`;
-        const chatRes = await fetch(chatResourceUrl);
-        const chatData = await chatRes.json();
-        return chatData.hubs;
-    }
+export const load: Load = async ({ fetch }) => {
+	try {
+		const chatResourceUrl = `${resourceUrl}/chat`;
+		const messageResourceUrl = `${resourceUrl}/chat/F2yEZsidaaIA3gKHHZoIj/zt4hyS3bpE5SM0agIGmDM`;
 
-    const messageRequest = async () => {
-        const messageResourceUrl = `${resourceUrl}/chat/F2yEZsidaaIA3gKHHZoIj/zt4hyS3bpE5SM0agIGmDM`;
-        const messageRes = await fetch(messageResourceUrl);
-        const messageData = await messageRes.json();
-        return messageData.messages;
-    }
-    
-    if (chatRequest && messageRequest) {
-        return {
-            status: 200,
-            hubs: await chatRequest(),
-            messages: await messageRequest()
-        };
-    }
-    return error(500, "Couldn't load hubs");
+		const chatRes = fetch(chatResourceUrl).then((res) => res.json());
+		const messageRes = fetch(messageResourceUrl).then((res) => res.json());
+
+		const [chatData, messageData] = await Promise.all([chatRes, messageRes]);
+        
+		return {
+			status: 200,
+			hubs: chatData.hubs,
+			messages: messageData.messages
+		};
+	} catch (err) {
+		return error(500, "Couldn't load hubs");
+	}
 };
